c19b242d836ce5d6aa8b02dc68c19a47b8bf7378
dohyu <dohyu@naver.com>
2021-07-11 20:53:14 +0900

    질문 목록과 질문 상세 기능 만들기

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,10 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    return 'Pybo index'
+    return redirect(url_for('question._list'))
 
 @bp.route('/hello')
 def hello_pybo():
